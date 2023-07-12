9042974a1e0c3ebfa2a34c8ae128ccf9dc88f5ea
0x19 <nevio@eiger.co>
2023-07-12 20:32:57 +0200

    Bytecode general godoc

--- bytecode/doc.go
+++ bytecode/doc.go
@@ -1,1 +1,5 @@
+/*
+Package bytecode provides tools for decoding and analyzing Ethereum contract, transaction, event and log bytecode.
+The package is designed to extract and interpret metadata from Ethereum contract creation bytecode. It provides a Metadata struct that represents the metadata contained in the bytecode, as defined by the Solidity compiler. This includes information such as the IPFS hash of the metadata, the Swarm hash of the metadata, experimental metadata, and the version of the Solidity compiler used.
+*/
 package bytecode
