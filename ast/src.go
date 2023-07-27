385368a3827c6a3ec7f1f56f075e5df81222b379
0x19 <nevio@eiger.co>
2023-07-27 11:45:13 +0200

    Adding SrcNode documentation and code cleanup

--- ast/src.go
+++ ast/src.go
@@ -4,78 +4,61 @@ import (
 	ast_pb "github.com/txpull/protos/dist/go/ast"
 )
 
+// SrcNode represents a node in the source code.
 type SrcNode struct {
-	// Id is the unique identifier of the source node.
-	Id int64 `json:"id"`
-	// Line is the line of the source node.
-	Line int64 `json:"line"`
-	// Column is the column of the source node.
-	Column int64 `json:"column"`
-	// Start is the start of the source node.
-	Start int64 `json:"start"`
-	// End is the end of the source node.
-	End int64 `json:"end"`
-	// Length is the length of the source node.
-	Length int64 `json:"length"`
-	// ParentIndex is the index of the parent node.
-	ParentIndex int64 `json:"parent_index"`
+	Id          int64 `json:"id"`           // Unique identifier of the source node.
+	Line        int64 `json:"line"`         // Line number of the source node in the source code.
+	Column      int64 `json:"column"`       // Column number of the source node in the source code.
+	Start       int64 `json:"start"`        // Start position of the source node in the source code.
+	End         int64 `json:"end"`          // End position of the source node in the source code.
+	Length      int64 `json:"length"`       // Length of the source node in the source code.
+	ParentIndex int64 `json:"parent_index"` // Index of the parent node in the source code.
 }
 
-func NewSrcNode(id int64, line int64, column int64, start int64, end int64, length int64, parentIndex int64) SrcNode {
-	return SrcNode{
-		Id:          id,
-		Line:        line,
-		Column:      column,
-		Start:       start,
-		End:         end,
-		Length:      length,
-		ParentIndex: parentIndex,
-	}
-}
-
-// GetId returns the ID of the source node.
+// GetId returns the unique identifier of the source node.
 func (s SrcNode) GetId() int64 {
 	return s.Id
 }
 
-// GetLine returns the line of the source node.
+// GetLine returns the line number of the source node in the source code.
 func (s SrcNode) GetLine() int64 {
 	return s.Line
 }
 
-// GetColumn returns the column of the source node.
+// GetColumn returns the column number of the source node in the source code.
 func (s SrcNode) GetColumn() int64 {
 	return s.Column
 }
 
-// GetStart returns the start of the source node.
+// GetStart returns the start position of the source node in the source code.
 func (s SrcNode) GetStart() int64 {
 	return s.Start
 }
 
-// GetEnd returns the end of the source node.
+// GetEnd returns the end position of the source node in the source code.
 func (s SrcNode) GetEnd() int64 {
 	return s.End
 }
 
-// GetLength returns the length of the source node.
+// GetLength returns the length of the source node in the source code.
 func (s SrcNode) GetLength() int64 {
 	return s.Length
 }
 
-// GetParentIndex returns the index of the parent node.
+// GetParentIndex returns the index of the parent node in the source code.
 func (s SrcNode) GetParentIndex() int64 {
 	return s.ParentIndex
 }
 
+// ToProto converts the SrcNode to a protocol buffer representation.
 func (s SrcNode) ToProto() *ast_pb.Src {
 	return &ast_pb.Src{
-		//Id:          s.Id,
-		Line:        s.Line,
-		Column:      s.Column,
-		Start:       s.Start,
-		End:         s.End,
-		Length:      s.Length,
-		ParentIndex: s.ParentIndex,
+		Id:          s.GetId(),
+		Line:        s.GetLine(),
+		Column:      s.GetColumn(),
+		Start:       s.GetStart(),
+		End:         s.GetEnd(),
+		Length:      s.GetLength(),
+		ParentIndex: s.GetParentIndex(),
 	}
 }
