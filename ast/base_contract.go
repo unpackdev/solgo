e75e81934ce4c3ae516f7b08b09d56614cfeec2f
0x19 <nevio@eiger.co>
2023-07-28 10:41:42 +0200

    Base contract documentation and cleanup

--- ast/base_contract.go
+++ ast/base_contract.go
@@ -4,25 +4,37 @@ import (
 	ast_pb "github.com/txpull/protos/dist/go/ast"
 )
 
+// BaseContract represents a base contract in a Solidity source file.
+// A base contract is a contract that is inherited by another contract.
 type BaseContract struct {
-	Id       int64             `json:"id"`
-	NodeType ast_pb.NodeType   `json:"node_type"`
-	Src      SrcNode           `json:"src"`
+	// Id is the unique identifier of the base contract.
+	Id int64 `json:"id"`
+	// NodeType is the type of the node.
+	// For a BaseContract, this is always NodeType_BASE_CONTRACT.
+	NodeType ast_pb.NodeType `json:"node_type"`
+	// Src contains source information about the node, such as its line and column numbers in the source file.
+	Src SrcNode `json:"src"`
+	// BaseName is the name of the base contract.
 	BaseName *BaseContractName `json:"base_name"`
 }
 
+// GetId returns the unique identifier of the base contract.
 func (b *BaseContract) GetId() int64 {
 	return b.Id
 }
 
+// GetType returns the type of the node.
+// For a BaseContract, this is always NodeType_BASE_CONTRACT.
 func (b *BaseContract) GetType() ast_pb.NodeType {
 	return b.NodeType
 }
 
+// GetSrc returns source information about the node, such as its line and column numbers in the source file.
 func (b *BaseContract) GetSrc() SrcNode {
 	return b.Src
 }
 
+// ToProto returns the protobuf representation of the base contract.
 func (b *BaseContract) ToProto() *ast_pb.BaseContract {
 	return &ast_pb.BaseContract{
 		Id:       b.Id,
@@ -32,26 +44,38 @@ func (b *BaseContract) ToProto() *ast_pb.BaseContract {
 	}
 }
 
+// BaseContractName represents the name of a base contract in a Solidity source file.
 type BaseContractName struct {
-	Id                    int64           `json:"id"`
-	NodeType              ast_pb.NodeType `json:"node_type"`
-	Src                   SrcNode         `json:"src"`
-	Name                  string          `json:"name"`
-	ReferencedDeclaration int64           `json:"referenced_declaration"`
+	// Id is the unique identifier of the base contract name.
+	Id int64 `json:"id"`
+	// NodeType is the type of the node.
+	// For a BaseContractName, this is always NodeType_BASE_CONTRACT_NAME.
+	NodeType ast_pb.NodeType `json:"node_type"`
+	// Src contains source information about the node, such as its line and column numbers in the source file.
+	Src SrcNode `json:"src"`
+	// Name is the name of the base contract.
+	Name string `json:"name"`
+	// ReferencedDeclaration is the unique identifier of the contract declaration that this name references.
+	ReferencedDeclaration int64 `json:"referenced_declaration"`
 }
 
+// GetId returns the unique identifier of the base contract name.
 func (b *BaseContractName) GetId() int64 {
 	return b.Id
 }
 
+// GetType returns the type of the node.
+// For a BaseContractName, this is always NodeType_BASE_CONTRACT_NAME.
 func (b *BaseContractName) GetType() ast_pb.NodeType {
 	return b.NodeType
 }
 
+// GetSrc returns source information about the node, such as its line and column numbers in the source file.
 func (b *BaseContractName) GetSrc() SrcNode {
 	return b.Src
 }
 
+// ToProto returns the protobuf representation of the base contract name.
 func (b *BaseContractName) ToProto() *ast_pb.BaseContractName {
 	return &ast_pb.BaseContractName{
 		Id:                    b.Id,
