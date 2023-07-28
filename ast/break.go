6ca509b1791d9bcac482a73f64a6b0ed7c4dd34b
0x19 <nevio@eiger.co>
2023-07-28 10:53:58 +0200

    Documentation, proto and cleanup of break.go

--- ast/break.go
+++ ast/break.go
@@ -5,46 +5,61 @@ import (
 	"github.com/txpull/solgo/parser"
 )
 
+// BreakStatement represents a 'break' statement in Solidity.
 type BreakStatement struct {
-	*ASTBuilder
+	*ASTBuilder // Embedding ASTBuilder for building the AST.
 
-	Id       int64           `json:"id"`
-	NodeType ast_pb.NodeType `json:"node_type"`
-	Src      SrcNode         `json:"src"`
+	Id       int64           `json:"id"`        // Unique identifier for the break statement.
+	NodeType ast_pb.NodeType `json:"node_type"` // Type of the node, which is 'BREAK' for a break statement.
+	Src      SrcNode         `json:"src"`       // Source information about the node, such as its line and column numbers in the source file.
 }
 
+// NewBreakStatement creates a new BreakStatement instance.
 func NewBreakStatement(b *ASTBuilder) *BreakStatement {
 	return &BreakStatement{
-		ASTBuilder: b,
-		Id:         b.GetNextID(),
-		NodeType:   ast_pb.NodeType_BREAK,
+		ASTBuilder: b,                     // ASTBuilder instance for building the AST.
+		Id:         b.GetNextID(),         // Generating a new unique identifier for the break statement.
+		NodeType:   ast_pb.NodeType_BREAK, // Setting the node type to 'BREAK'.
 	}
 }
 
+// GetId returns the unique identifier of the break statement.
 func (b *BreakStatement) GetId() int64 {
 	return b.Id
 }
 
+// GetType returns the type of the node, which is 'BREAK' for a break statement.
 func (b *BreakStatement) GetType() ast_pb.NodeType {
 	return b.NodeType
 }
 
+// GetSrc returns the source information about the node, such as its line and column numbers in the source file.
 func (b *BreakStatement) GetSrc() SrcNode {
 	return b.Src
 }
 
+// GetTypeDescription returns the type description of the break statement.
+// As the break statement doesn't have a type description, it returns nil.
 func (b *BreakStatement) GetTypeDescription() *TypeDescription {
 	return nil
 }
 
+// GetNodes returns the child nodes of the break statement.
+// As the break statement doesn't have any child nodes, it returns nil.
 func (b *BreakStatement) GetNodes() []Node[NodeType] {
 	return nil
 }
 
+// ToProto returns the protobuf representation of the break statement.
 func (b *BreakStatement) ToProto() NodeType {
-	return ast_pb.Break{}
+	return ast_pb.Break{
+		Id:       b.Id,
+		NodeType: b.NodeType,
+		Src:      b.Src.ToProto(),
+	}
 }
 
+// Parse populates the BreakStatement fields using the provided parser context.
 func (b *BreakStatement) Parse(
 	unit *SourceUnit[Node[ast_pb.SourceUnit]],
 	contractNode Node[NodeType],
