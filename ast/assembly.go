fa5251b9a53444e82ad9dc3d59e676e956c48c89
0x19 <nevio@eiger.co>
2023-07-28 10:38:30 +0200

    Assembly statement cleanup and docs, including warning

--- ast/assembly.go
+++ ast/assembly.go
@@ -5,15 +5,23 @@ import (
 	"github.com/txpull/solgo/parser"
 )
 
+// AssemblyStatement represents an assembly statement in a Solidity source file.
+// @WARN: AssemblyStatement is not yet implemented.
 type AssemblyStatement struct {
 	*ASTBuilder
 
-	Id       int64           `json:"id"`
+	// Id is the unique identifier of the assembly statement.
+	Id int64 `json:"id"`
+	// NodeType is the type of the node.
+	// For an AssemblyStatement, this is always NodeType_ASSEMBLY_STATEMENT.
 	NodeType ast_pb.NodeType `json:"node_type"`
-	Src      SrcNode         `json:"src"`
-	Body     *BodyNode       `json:"body"`
+	// Src contains source information about the node, such as its line and column numbers in the source file.
+	Src SrcNode `json:"src"`
+	// Body is the body of the assembly statement, represented as a BodyNode.
+	Body *BodyNode `json:"body"`
 }
 
+// NewAssemblyStatement creates a new AssemblyStatement.
 func NewAssemblyStatement(b *ASTBuilder) *AssemblyStatement {
 	return &AssemblyStatement{
 		ASTBuilder: b,
@@ -22,34 +30,44 @@ func NewAssemblyStatement(b *ASTBuilder) *AssemblyStatement {
 	}
 }
 
+// GetId returns the unique identifier of the assembly statement.
 func (a *AssemblyStatement) GetId() int64 {
 	return a.Id
 }
 
+// GetType returns the type of the node.
+// For an AssemblyStatement, this is always NodeType_ASSEMBLY_STATEMENT.
 func (a *AssemblyStatement) GetType() ast_pb.NodeType {
 	return a.NodeType
 }
 
+// GetSrc returns source information about the node, such as its line and column numbers in the source file.
 func (a *AssemblyStatement) GetSrc() SrcNode {
 	return a.Src
 }
 
+// GetBody returns the body of the assembly statement, represented as a BodyNode.
 func (a *AssemblyStatement) GetBody() *BodyNode {
 	return a.Body
 }
 
+// GetNodes returns the statements in the body of the assembly statement.
 func (a *AssemblyStatement) GetNodes() []Node[NodeType] {
 	return a.Body.Statements
 }
 
+// GetTypeDescription returns the type description of the assembly statement.
+// For an AssemblyStatement, this is always nil.
 func (a *AssemblyStatement) GetTypeDescription() *TypeDescription {
 	return nil
 }
 
+// ToProto returns the protobuf representation of the assembly statement.
 func (a *AssemblyStatement) ToProto() NodeType {
 	return ast_pb.Statement{}
 }
 
+// Parse parses an AssemblyStatementContext to populate the fields of the AssemblyStatement.
 func (a *AssemblyStatement) Parse(
 	unit *SourceUnit[Node[ast_pb.SourceUnit]],
 	contractNode Node[NodeType],
