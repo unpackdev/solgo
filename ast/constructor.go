71c4ce1b50694e3e90a6009503645129462ceb98
0x19 <nevio@eiger.co>
2023-07-28 11:08:24 +0200

    Documentation and cleanup of constructor

--- ast/constructor.go
+++ ast/constructor.go
@@ -5,22 +5,46 @@ import (
 	"github.com/txpull/solgo/parser"
 )
 
+// The Constructor struct represents a constructor function in a Solidity contract.
 type Constructor struct {
+	// Embedding the ASTBuilder to provide common functionality
 	*ASTBuilder
 
-	Id               int64             `json:"id"`
-	NodeType         ast_pb.NodeType   `json:"node_type"`
-	Src              SrcNode           `json:"src"`
-	Kind             ast_pb.NodeType   `json:"kind"`
-	StateMutability  ast_pb.Mutability `json:"state_mutability"`
-	Visibility       ast_pb.Visibility `json:"visibility"`
-	Implemented      bool              `json:"implemented"`
-	Parameters       *ParameterList    `json:"parameters"`
-	ReturnParameters *ParameterList    `json:"return_parameters"`
-	Scope            int64             `json:"scope"`
-	Body             *BodyNode         `json:"body"`
+	// The unique identifier for the constructor
+	Id int64 `json:"id"`
+
+	// The type of the node, which is 'FUNCTION_DEFINITION' for a constructor
+	NodeType ast_pb.NodeType `json:"node_type"`
+
+	// The source information about the constructor, such as its line and column numbers in the source file
+	Src SrcNode `json:"src"`
+
+	// The kind of the node, which is 'CONSTRUCTOR' for a constructor
+	Kind ast_pb.NodeType `json:"kind"`
+
+	// The state mutability of the constructor, which is 'NONPAYABLE' by default
+	StateMutability ast_pb.Mutability `json:"state_mutability"`
+
+	// The visibility of the constructor
+	Visibility ast_pb.Visibility `json:"visibility"`
+
+	// Whether the constructor is implemented or not
+	Implemented bool `json:"implemented"`
+
+	// The parameters of the constructor
+	Parameters *ParameterList `json:"parameters"`
+
+	// The return parameters of the constructor, which are always empty for a constructor
+	ReturnParameters *ParameterList `json:"return_parameters"`
+
+	// The scope of the constructor, which is the id of the contract that the constructor belongs to
+	Scope int64 `json:"scope"`
+
+	// The body of the constructor, which is a block of statements
+	Body *BodyNode `json:"body"`
 }
 
+// NewConstructor creates a new Constructor instance.
 func NewConstructor(b *ASTBuilder) *Constructor {
 	return &Constructor{
 		ASTBuilder:      b,
@@ -31,62 +55,77 @@ func NewConstructor(b *ASTBuilder) *Constructor {
 	}
 }
 
+// GetId returns the unique identifier of the constructor.
 func (c *Constructor) GetId() int64 {
 	return c.Id
 }
 
+// GetSrc returns the source information about the constructor.
 func (c *Constructor) GetSrc() SrcNode {
 	return c.Src
 }
 
+// GetType returns the type of the node, which is 'FUNCTION_DEFINITION' for a constructor.
 func (c *Constructor) GetType() ast_pb.NodeType {
 	return c.NodeType
 }
 
+// GetNodes returns the statements in the body of the constructor.
 func (c *Constructor) GetNodes() []Node[NodeType] {
 	return c.Body.Statements
 }
 
+// GetTypeDescription returns the type description of the constructor, which is nil as constructors do not have a type description.
 func (c *Constructor) GetTypeDescription() *TypeDescription {
 	return nil
 }
 
+// GetParameters returns the parameters of the constructor.
 func (c *Constructor) GetParameters() *ParameterList {
 	return c.Parameters
 }
 
+// GetReturnParameters returns the return parameters of the constructor, which are always empty for a constructor.
 func (c *Constructor) GetReturnParameters() *ParameterList {
 	return c.ReturnParameters
 }
 
+// GetBody returns the body of the constructor.
 func (c *Constructor) GetBody() *BodyNode {
 	return c.Body
 }
 
+// GetKind returns the kind of the node, which is 'CONSTRUCTOR' for a constructor.
 func (c *Constructor) GetKind() ast_pb.NodeType {
 	return c.Kind
 }
 
+// IsImplemented returns whether the constructor is implemented or not.
 func (c *Constructor) IsImplemented() bool {
 	return c.Implemented
 }
 
+// GetVisibility returns the visibility of the constructor.
 func (c *Constructor) GetVisibility() ast_pb.Visibility {
 	return c.Visibility
 }
 
+// GetStateMutability returns the state mutability of the constructor.
 func (c *Constructor) GetStateMutability() ast_pb.Mutability {
 	return c.StateMutability
 }
 
+// GetScope returns the scope of the constructor, which is the id of the contract that the constructor belongs to.
 func (c *Constructor) GetScope() int64 {
 	return c.Scope
 }
 
+// ToProto returns the protobuf representation of the constructor.
 func (c *Constructor) ToProto() NodeType {
 	return ast_pb.Function{}
 }
 
+// Parse parses a constructor from the provided parser.ConstructorDefinitionContext and returns the corresponding Constructor.
 func (c *Constructor) Parse(
 	unit *SourceUnit[Node[ast_pb.SourceUnit]],
 	contractNode Node[NodeType],
@@ -144,13 +183,13 @@ func (c *Constructor) Parse(
 	return c
 }
 
+// getVisibilityFromCtx returns the visibility of the constructor based on the provided parser.ConstructorDefinitionContext.
 func (c *Constructor) getVisibilityFromCtx(ctx *parser.ConstructorDefinitionContext) ast_pb.Visibility {
 	visibilityMap := map[string]ast_pb.Visibility{
 		"public":   ast_pb.Visibility_PUBLIC,
 		"internal": ast_pb.Visibility_INTERNAL,
 	}
 
-	// Check each visibility context in the map
 	if len(ctx.AllPublic()) > 0 {
 		if v, ok := visibilityMap["public"]; ok {
 			return v
@@ -161,6 +200,5 @@ func (c *Constructor) getVisibilityFromCtx(ctx *parser.ConstructorDefinitionCont
 		}
 	}
 
-	// If no visibility context matches, return the default value
 	return ast_pb.Visibility_INTERNAL
 }
