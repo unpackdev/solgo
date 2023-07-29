18c0fdb90c882c25dbd119fb357299e0469efd1e
0x19 <nevio@eiger.co>
2023-07-29 14:20:07 +0200

    sources.go documentation

--- sources.go
+++ sources.go
@@ -6,17 +6,21 @@ import (
 	"strings"
 )
 
+// SourceUnit represents a unit of source code in Solidity. It includes the name, path, and content of the source code.
 type SourceUnit struct {
 	Name    string `yaml:"name" json:"name"`
 	Path    string `yaml:"path" json:"path"`
 	Content string `yaml:"content" json:"content"`
 }
 
+// Sources represents a collection of SourceUnit. It includes a slice of SourceUnit and the name of the entry source unit.
 type Sources struct {
 	SourceUnits         []SourceUnit `yaml:"source_units" json:"source_units"`
 	EntrySourceUnitName string       `yaml:"entry_source_unit" json:"base_source_unit"`
 }
 
+// Prepare validates and prepares the Sources. It checks if each SourceUnit has either a path or content and a name.
+// If a SourceUnit has a path but no content, it reads the content from the file at the path.
 func (s Sources) Prepare() error {
 	for _, sourceUnit := range s.SourceUnits {
 		if sourceUnit.Path == "" && sourceUnit.Content == "" {
@@ -39,6 +43,7 @@ func (s Sources) Prepare() error {
 	return nil
 }
 
+// GetCombinedSource combines the content of all SourceUnits in the Sources into a single string, separated by two newlines.
 func (s Sources) GetCombinedSource() string {
 	var sources []string
 	for _, sourceUnit := range s.SourceUnits {
@@ -48,6 +53,7 @@ func (s Sources) GetCombinedSource() string {
 	return strings.Join(sources, "\n\n")
 }
 
+// GetSourceUnitByName returns the SourceUnit with the given name from the Sources. If no such SourceUnit exists, it returns nil.
 func (s Sources) GetSourceUnitByName(name string) *SourceUnit {
 	for _, sourceUnit := range s.SourceUnits {
 		if sourceUnit.Name == name {
@@ -57,6 +63,7 @@ func (s Sources) GetSourceUnitByName(name string) *SourceUnit {
 	return nil
 }
 
+// GetSourceUnitByPath returns the SourceUnit with the given path from the Sources. If no such SourceUnit exists, it returns nil.
 func (s Sources) GetSourceUnitByPath(path string) *SourceUnit {
 	for _, sourceUnit := range s.SourceUnits {
 		if sourceUnit.Path == path {
