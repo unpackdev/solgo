8daeefa278b2a1b065f0d49728383adc4f85a795
0x19 <nevio@eiger.co>
2023-08-14 23:25:30 +0200

    Ups... Bringing back G204 nosec

--- audit/slither.go
+++ audit/slither.go
@@ -93,6 +93,8 @@ func (s *Slither) Analyze(sources *solgo.Sources) (*Response, []byte, error) {
 		return nil, nil, err
 	}
 
+	// #nosec G204
+	// G204 (CWE-78): Subprocess launched with variable (Confidence: HIGH, Severity: MEDIUM)
 	cmd := exec.CommandContext(s.ctx, "slither", args...)
 	output, err := cmd.CombinedOutput()
 
