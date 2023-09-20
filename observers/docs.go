f36fae555c45d495933310912bf427e9784aef92
Nevio Vesic <0x19@users.noreply.github.com>
2023-09-20 20:36:24 +0200

    Observers package - Block Subscriber (#103)
    * BlockSubscriber Header and Full Block
    * Fixing workflows
    * Docs.go

---
+++ observers/docs.go
@@ -0,0 +1,5 @@
+// Package observers provides tools for managing and interacting with Ethereum onchain data such as blocks, transactions, events and logs.
+// It provides a BlockSubscriber structure that allows for subscribing to block headers based on various criteria, such as the latest block or a range of blocks.
+// It also provides a TransactionSubscriber structure that allows for subscribing to transactions based on various criteria, such as the latest transactions or a range of transactions.
+// The package is designed to be flexible and efficient, ensuring that Ethereum onchain data can be easily accessed based on the specific needs of the application.
+package observers
