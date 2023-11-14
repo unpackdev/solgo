5914c2ec7ddf7347bbe1971750b92a3d53967cff
Nevio Vesic <0x19@users.noreply.github.com>
2023-11-14 11:00:26 +0100

    Unit Test Path Fixes (#136)

--- data/tests/contracts/1:0x3b07A1A5de80f9b22DE0EC6C44C6E59DDc1C5f41/IRootChainManager.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity =0.8.16;
-
-interface IRootChainManager {
-    function depositFor(address user, address rootToken, bytes calldata depositData) external;
-
-    function exit(bytes memory _data) external;
-}
