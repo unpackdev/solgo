e6bc02244fb2abec8a459e00ae43129759a343c5
Evan Chng Xian Lin <xianlinchng@gmail.com>
2024-04-14 23:29:26 +0800

    fix edge case in printing strings

--- printer/ast_printer/primary_expression.go
+++ printer/ast_printer/primary_expression.go
@@ -3,11 +3,18 @@ package ast_printer
 import (
 	"strings"
 
+	ast_pb "github.com/unpackdev/protos/dist/go/ast"
 	"github.com/unpackdev/solgo/ast"
 )
 
 func printPrimaryExpression(node *ast.PrimaryExpression, sb *strings.Builder, depth int) bool {
 	s := ""
+	if node.GetKind() == ast_pb.NodeType_UNICODE_STRING_LITERAL {
+		s = "\"" + node.GetValue() + "\""
+		sb.WriteString(s)
+		return true
+	}
+
 	if node.GetValue() == "" {
 		s = node.GetName()
 	} else {
